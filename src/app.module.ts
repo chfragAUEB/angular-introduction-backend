73a0e894d16beaaab730a94ca4bbe7c9062dd859
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2023-10-31 19:08:58 +0200

    mongodb atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://dbadmin:<password>@cluster0.okry00y.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
